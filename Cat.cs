0f93005cf6f99a9b9044494ec534f16ef26c4cdb
Gihun Ham <progh2@gmail.com>
2025-06-11 13:59:26 +0900

    강아지 고양이 클래스 예제

---
+++ Cat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyExample32
+{
+    class Cat
+    {
+        public Cat() { this.Age = 0; }
+
+        public int Age { get; set; }
+
+        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+        public void Meow() { Console.WriteLine("냥냥 웁니다."); }
+    }
+}
